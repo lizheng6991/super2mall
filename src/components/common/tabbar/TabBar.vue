dffe5bdb31c0b1581b5ef1d6a221e9bfbc24c1f9
lizheng6991 <1173801657@qq.com>
2022-08-15 14:25:05 +0800

    测试

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name: 'TabBar',
+  data () {
+    return {
+      
+    };
+  },
+}
+</script>
+
+<style scpoed>
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name: 'TabBarItem',
+  data () {
+    return {
+      
+    };
+  },
+}
+</script>
+
+<style scpoed>
+</style>
